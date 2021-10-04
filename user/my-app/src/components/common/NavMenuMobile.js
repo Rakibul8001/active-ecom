69451a600e3ef3eb8eab4cf6f5afa8ba3769a49d
Rakibul8001 <ghumpori8001@gmail.com>
2021-10-05 00:09:10 +0600

    Add NavmenuMobile.js

---
+++ user/my-app/src/components/common/NavMenuMobile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavMenuMobile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is NavMenuMobile component</h1>
+            </div>
+        );
+    }
+}
+
+export default NavMenuMobile;
