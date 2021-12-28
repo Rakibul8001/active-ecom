2d78b70a9f3b8a191a43ba1b9a288086b11a1045
Rakibul8001 <ghumpori8001@gmail.com>
2021-12-28 23:28:00 +0600

    initial setup

---
+++ admin/storage/framework/views/2dffd9f446fd8ce6c584adadf009a4aa1ec439a9.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Ecommerce Admin</title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>";
+</head>
+<body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<div id="root"></div>
+<script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH E:\PWA\admin\resources\views/index.blade.php ENDPATH**/ ?>
